b79b671746eec675fac0fc1f6a8b007767dbc5ca
Mario Sánchez <msanchez@trovit.com>
2018-06-15 09:38:21 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var quotes = [
+  "Hijos míos, os habéis esforzado, ¿y para qué? Para hacer el ridículo. La moraleja es: ¡no os esforcéis!"
+];
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function homer() {
+  return quotes[getRandomInt(0, quotes.length)];
+}
+
+console.log(homer());
