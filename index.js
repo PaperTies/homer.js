3b6fa75e37562b89bb21b7d25c73b2524f3739f2
alexhoma <alexcm.14@gmail.com>
2018-06-16 15:44:35 +0200

    add 2 extra cool quotes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var quotes = [
   'Hijos míos, os habéis esforzado, ¿y para qué? Para hacer el ridículo. La moraleja es: ¡no os esforcéis!',
+  'Marge, ¿dónde está...? esa cosa... eso que se usa... ¡taca, y a comer!',
+  'Bueno, tendrá todo el dinero del mundo, pero hay algo que nunca podrá comprar Marge. ¡Un dinosaurio!',
 ];
 
 function getRandomInt(min, max) {
