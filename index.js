42715a9159c1da4b872958d6955681140356cdbf
Mario Sánchez <msanchez@trovit.com>
2018-06-18 12:04:46 +0200

    chore: separate quotes in a .json file

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-var quotes = [
-  'Hijos míos, os habéis esforzado, ¿y para qué? Para hacer el ridículo. La moraleja es: ¡no os esforcéis!',
-  'Marge, ¿dónde está...? esa cosa... eso que se usa... ¡taca, y a comer!',
-  'Bueno, tendrá todo el dinero del mundo, pero hay algo que nunca podrá comprar Marge. ¡Un dinosaurio!',
-];
+const fs = require("fs");
+let quotes = JSON.parse(fs.readFileSync("quotes.json"));
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
